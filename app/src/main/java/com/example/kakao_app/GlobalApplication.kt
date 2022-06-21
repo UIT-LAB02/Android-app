cabec400c0ed3e68293e2dfb1da7973c6eaf42a9
imjaeseong <zks145@naver.com>
2022-06-21 22:01:54 +0900

    git commit second

---
+++ app/src/main/java/com/example/kakao_app/GlobalApplication.kt
@@ -0,0 +1,13 @@
+package com.example.kakao_app
+
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+
+class GlobalApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+
+        KakaoSdk.init(this, "a81f58772bd67ab645f65c8b16bc07e3")
+    }
+
+}
---
+++ app/src/main/java/com/example/kakao_app/SecondActivity.kt
@@ -0,0 +1,60 @@
+package com.example.kakao_app
+
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.kakao.sdk.user.UserApiClient
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.secondactivity)
+
+        var id : TextView = findViewById(R.id.id)
+        var nickname : TextView = findViewById(R.id.nickname)
+        var profileimage_url : TextView = findViewById(R.id.profileimage_url)
+        var thumbnailimage_url : TextView = findViewById(R.id.thumbnailimage_url)
+
+        UserApiClient.instance.me { user, error ->
+            id.text = "회원번호: ${user?.id}"
+            nickname.text = "닉네임: ${user?.kakaoAccount?.profile?.nickname}"
+            profileimage_url.text = "프로필 링크: ${user?.kakaoAccount?.profile?.profileImageUrl}"
+            thumbnailimage_url.text = "썸네일 링크: ${user?.kakaoAccount?.profile?.thumbnailImageUrl}"
+        }
+
+        val kakao_logout_button = findViewById<Button>(R.id.kakao_logout_button) // 로그인 버튼
+
+        kakao_logout_button.setOnClickListener {
+            UserApiClient.instance.logout { error ->
+                if (error != null) {
+                    Toast.makeText(this, "로그아웃 실패 $error", Toast.LENGTH_SHORT).show()
+                }else {
+                    Toast.makeText(this, "로그아웃 성공", Toast.LENGTH_SHORT).show()
+                }
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP))
+                finish()
+            }
+        }
+
+        val kakao_unlink_button = findViewById<Button>(R.id.kakao_unlink_button) // 로그인 버튼
+
+        kakao_unlink_button.setOnClickListener {
+            UserApiClient.instance.unlink { error ->
+                if (error != null) {
+                    Toast.makeText(this, "회원 탈퇴 실패 $error", Toast.LENGTH_SHORT).show()
+                }else {
+                    Toast.makeText(this, "회원 탈퇴 성공", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP))
+                    finish()
+                }
+            }
+        }
+    }
+
+}
